e3ee85b0fe699e613eee3c07070db877360a3d7d
Laurence <laurencehall1@outlook.com>
2019-05-07 20:52:13 -0400

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use std::env;
-use std::fs;
-
+use std::{env, error::Error, fs, process};
+use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
-
 
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
@@ -12,22 +10,9 @@ fn main() {
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
-
-    let contents =
-        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
-}
-struct Config {
-    query: String,
-    filename: String,
-}
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            panic!("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
     }
 }
+
